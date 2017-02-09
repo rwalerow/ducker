7dd377ac004a706d0f27bcdb07271bf49e9da430
Robert <viniolli@gmail.com>
2017-02-09 21:29:05 +0100

    Ready priject from video 9 | hmr doesnt work

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const MainContainer = React.createClass({
-	render (){
-		return (
-			<p>Hello World!</p>
-		)
-	}
+  render () {
+    return (
+      <p>{'Hello World 2ssdfsdfdf3sadfsd'}</p>
+    )
+  }
 })
 
 export default MainContainer
