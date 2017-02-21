79606790c4128c0050cce79f35862ee701872054
Robert <viniolli@gmail.com>
2017-02-21 23:56:53 +0100

    course 3 video 12 - First component connected to redux

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import { connect } from 'react-redux'
+import * as userActions from 'redux/modules/users'
+import { bindActionCreators } from 'redux'
 
 const AuthenticateContainer = React.createClass({
+  propTypes: {
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+    authUser: PropTypes.func.isRequired,
+    fetchingUser: PropTypes.func.isRequired,
+    fetchingUserSuccess: PropTypes.func.isRequired,
+    fetchinUserFailure: PropTypes.func.isRequired
+  },
   handleAuth () {
+    this.props.fetchingUser()
     auth().then((user) => {
-      console.log('Auth user', user)
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+      this.props.authUser(user.uid)
     })
+    .catch((err) => this.props.fetchinUserFailure())
   },
   render () {
     return (
       <Authenticate
-        isFetching={false}
-        error=''
+        isFetching={this.props.isFetching}
+        error={this.props.error}
         onAuth={this.handleAuth}/>
     )
   }
 })
 
-export default AuthenticateContainer
+function mapStateToProps(state) {
+  return {
+    isFetching: state.isFetching,
+    error: state.error
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(userActions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)
