a7f16208aa7cda555610f9d0d72710282af15cb7
Robert <viniolli@gmail.com>
2017-03-09 17:10:42 +0100

    course 3 video 16 - add devtoolsExtension for a better redux feel

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom'
 import getRoutes from './config/routes'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import users from 'redux/modules/users'
 import { Provider } from 'react-redux'
 import React from 'react'
 import thunk from 'redux-thunk'
 import { checkIfAuthed } from 'helpers/auth'
 
-const store = createStore(users, applyMiddleware(thunk))
+const store = createStore(users, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+))
+
 
 function checkAuth(nextState, replace) {
   const isAuthed = checkIfAuthed(store)
